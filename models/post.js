e57664a5d1e011757442d44d2b8e1ffc9678879f
cogi234 <colinbougie@gmail.com>
2024-11-28 09:55:03 -0500

    Remove unused models/controllers and add owner foreign key to posts

--- models/post.js
+++ models/post.js
@@ -1,4 +1,6 @@
 import Model from './model.js';
+import Repository from './repository.js';
+import User from './user.js';
 
 export default class Post extends Model {
     constructor() {
@@ -9,7 +11,27 @@ export default class Post extends Model {
         this.addField('Category', 'string');
         this.addField('Image', 'asset');
         this.addField('Date', 'integer');
+        this.addField('OwnerId', 'string');
 
         this.setKey("Title");
+    }
+    
+    bindExtraData(instance) {
+        instance = super.bindExtraData(instance);
+        
+
+        usersRepository = new Repository(new UserModel());
+        owner = usersRepository.get(instance.OwnerId);
+
+        if (owner != null) {
+            instance.OwnerName = owner.Name;
+            instance.OwnerAvatar = owner.Avatar;
+        } else {
+            instance.OwnerName = null;
+            instance.OwnerAvatar = null;
+        }
+
+        instance.OwnerId = null;
+        return instance;
     }
 }
